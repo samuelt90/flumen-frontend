f7a4c89f541735adcef02553edb76a5a6761ace4
Samuel Torres <setorresg2015@gmail.com>
2026-01-02 21:01:04 -0600

    nuevo api_url

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/health`, {
-    cache: "no-store",
-  });
-
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/health`, {
+  cache: "no-store",
+});
   const data = await res.json();
 
   return (
